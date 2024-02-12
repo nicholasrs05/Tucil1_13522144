0f45d1d5f156a141e0a5067fb18e240b5166714d
Nicholas Sihite <nicholassihite05@gmail.com>
2024-02-12 11:52:12 +0700

    progres lagi

--- src/cyberpunkhack.c
+++ src/cyberpunkhack.c
@@ -1,15 +1,27 @@
 #include <stdlib.h>
+#include <time.h>
 #include "cyberpunkhack.h"
 #include "boolean.h"
 
 /* --- Realisasi Fungsi dan Prosedur --- */
-void createArray(strArray *Arr, int length){
-    (*Arr).buffer = (char**) malloc (length * sizeof(char*));
+void createIntArray(intArray *Arr, int length){
+    (*Arr).buffer = (int*) malloc (length * sizeof(int));
     (*Arr).nEff = 0;
+    (*Arr).maxLen = length;
 }
 
-void createMatrix(strMatrix *Mat, int height, int width){
+void createStringArray(strArray *Arr, int length){
+    (*Arr).buffer = (char**) malloc (length * sizeof(char*));
+    (*Arr).nEff = 0;
+    (*Arr).maxLen = length;
+}
+
+void createStringMatrix(strMatrix *Mat, int height, int width){
+    int i;
     (*Mat).buffer = (strArray*) malloc (height * sizeof(strArray));
+    for (i = 0; i < height; i++){
+        createStringArray(&(*Mat).buffer[i], width);
+    }
     (*Mat).row = height;
     (*Mat).col = width;
 }
@@ -19,10 +31,25 @@ void createStack(strStack *Stack, int capacity){
     (*Stack).top = -1;
 }
 
-void insertLast(strArray *Arr, char *str){
-    (*Arr).buffer[(*Arr).nEff] = (char*) malloc (100 * sizeof(char));
-    strcpy((*Arr).buffer[(*Arr).nEff], str);
-    (*Arr).nEff++;
+void createPoint(point *P, int X, int Y){
+    (*P).X = X;
+    (*P).Y = Y;
+}
+
+void createPointArray(pointArray *Arr, int length){
+    (*Arr).buffer = (point*) malloc (length * sizeof(point));
+    (*Arr).nEff = 0;
+    (*Arr).maxLen = length;
+}
+
+void insertLastStrArr(strArray *Arr, char *str){
+    if ((*Arr).nEff == (*Arr).maxLen){
+        printf("Array penuh!\n");
+    } else {
+        (*Arr).buffer[(*Arr).nEff] = (char*) malloc (100 * sizeof(char));
+        strcpy((*Arr).buffer[(*Arr).nEff], str);
+        (*Arr).nEff++;
+    }
 }
 
 void push(strStack *Stack, char *str){
@@ -35,30 +62,42 @@ void pop(strStack *Stack, char *str){
     (*Stack).top--;
 }
 
-void displayArray(strArray *Arr){
+void displayArray(strArray Arr){
     int i;
-    for (i = 0; i < (*Arr).nEff; i++){
-        printf("%s ", (*Arr).buffer[i]);
+    for (i = 0; i < Arr.nEff; i++){
+        printf("%s ", Arr.buffer[i]);
     }
     printf("\n");
 }
 
-void displayMatrix(strMatrix *Mat){
+void displayMatrix(strMatrix Mat){
     int i, j;
-    for (i = 0; i < (*Mat).row; i++){
-        for (j = 0; j < (*Mat).col; j++){
-            printf("%s ", (*Mat).buffer[i].buffer[j]);
+    for (i = 0; i < Mat.row; i++){
+        for (j = 0; j < Mat.col; j++){
+            printf("%s ", Mat.buffer[i].buffer[j]);
         }
         printf("\n");
     }
 }
 
+void displaySeqAndReward(strMatrix Mat, intArray Seq, intArray Reward){
+    int i, j;
+    for (i = 0; i < Mat.row; i++){
+        printf("Sequence %d: ", i + 1);
+        for (j = 0; j < Seq.buffer[i]; j++){
+            printf("%s ", Mat.buffer[i].buffer[j]);
+        }
+        printf("dengan reward: %d\n", Reward.buffer[i]);
+    }
+}
+
 /* --- Program Utama --- */
 int main(){
-    int i, j, k, bufferSize, mtxHeight, mtxWidth, cntSeq, option, cntToken, maxSeqSize;
-    strArray seqArray;
-    strMatrix seqReward, tokenMatrix;
-    char *Word;
+    int i, j, k, bufferSize, mtxHeight, mtxWidth, cntSeq, option, cntToken, maxSeqSize, randomInt, reward, tempSeqLength;
+    intArray seqSize, seqReward;
+    strArray seqArray, tempRow;
+    strMatrix sequences, tokenMatrix;
+    char Word[100];
     boolean valid;
 
     printf("Cyberpunk 2077 Breach Protocol Solver with Brute Force Algorithm\n\n");
@@ -81,18 +120,21 @@ int main(){
 
     if (option == 1){ //  Jika input dari file
         printf("Masukan Anda adalah dari file\n");
+        // something
+
     } else { //  Jika input dari CLI (setelah validasi, hanya mungkin option == 2)
         printf("Masukan Anda adalah dari CLI\n");
         printf("Masukkan banyaknya token unik yang ingin dijadikan matrix sequence\n");
         printf(">> "); scanf("%d", &cntToken);
         
         // membuat array untuk menyimpan token unik
-        createArray(&seqArray, cntToken);
+        createStringArray(&seqArray, cntToken);
 
+        // menerima input dari user
         printf("Masukkan token unik yang ingin dijadikan matrix sequence\n");
         for (i = 0; i < cntToken; i++){
             scanf("%s", Word);
-            insertLast(&seqArray, Word);
+            insertLastStrArr(&seqArray, Word);
         }
 
         printf("Masukkan ukuran buffer yang diinginkan\n");
@@ -106,6 +148,38 @@ int main(){
 
         printf("Masukkan ukuran maksimal sequence yang diinginkan\n");
         printf(">> "); scanf("%d", &maxSeqSize);
+
+        // membuat matrix random
+        createStringMatrix(&tokenMatrix, mtxHeight, mtxWidth);
+        srand(time(NULL));
+        for (i = 0; i < mtxHeight; i++){
+            for (j = 0; j < mtxWidth; j++){
+                randomInt = rand() % cntToken;
+                tokenMatrix.buffer[i].buffer[j] = seqArray.buffer[randomInt];
+            }
+        }
+        displayMatrix(tokenMatrix);
+
+        // membuat random sequence
+        createStringMatrix(&sequences, cntSeq, maxSeqSize + 2);
+        createIntArray(&seqSize, cntSeq);
+        createIntArray(&seqReward, cntSeq);
+        for (i = 0; i < cntSeq; i++){
+            tempSeqLength = (rand() % maxSeqSize) + 1;
+            seqSize.buffer[i] = tempSeqLength;
+            for (j = 0; j < tempSeqLength; j++){
+                randomInt = rand() % cntToken;
+                sequences.buffer[i].buffer[j] = seqArray.buffer[randomInt];
+            }
+            randomInt = ((rand() % 10) + 1) * 5; // hadiah sequence random berupa integer kelipatan 5 pada range 5 - 50
+            reward = randomInt;
+            randomInt = rand() % 2;
+            if (randomInt == 1){ // jika randomInt == 1, maka reward sequence negatif
+                reward = -reward;
+            }
+            seqReward.buffer[i] = reward;
+        }
+        displaySeqAndReward(sequences, seqSize, seqReward);
     }
 
     return 0;
@@ -116,25 +190,28 @@ int main(){
 //     strArray ArrayString;
 //     strMatrix MatrixString;
 //     strStack StackString;
+//     point P;
+//     pointArray ArrayPoint;
 //     char word[100];
-//     int i, j, arrLen, matRow, matCol, stackCap;
-//     arrLen = 2;
+//     int i, j, arrLen, matRow, matCol, stackCap, random;
+//     arrLen = 3;
 //     matRow = 2; 
 //     matCol = 2;
 //     stackCap = 10;
-//     createArray(&ArrayString, arrLen);
-//     createMatrix(&MatrixString, matRow, matCol);
+//     createStringArray(&ArrayString, arrLen);
+//     createStringMatrix(&MatrixString, matRow, matCol);
 //     createStack(&StackString, stackCap);
 //     for (i = 0; i < 2; i++){
 //         scanf("%s", word);
-//         insertLast(&ArrayString, word);
+//         insertLastStrArr(&ArrayString, word);
 //     }
 //     for (i = 0; i < 2; i++){
 //         printf("%s\n", ArrayString.buffer[i]);
 //     }
-//     for (i = 0; i < matRow; i++){
-//         MatrixString.buffer[i] = ArrayString;
-//     }
+//     MatrixString.buffer[0] = ArrayString;
+//     ArrayString.buffer[0] = "A";
+//     ArrayString.buffer[1] = "B";
+//     MatrixString.buffer[1] = ArrayString;
 //     for (i = 0; i < matRow; i++){
 //         for (j = 0; j < matCol; j++){
 //             printf("%s ", MatrixString.buffer[i].buffer[j]);
@@ -151,6 +228,30 @@ int main(){
 //     printf("%s\n", StackString.buffer[StackString.top]);
 //     pop(&StackString, word);
 //     printf("%s\n", StackString.buffer[StackString.top]);
+
+//     srand(time(NULL));
+//     random = rand();
+//     printf("%d\n", (random % 6) + 1);
+
+//     createStringMatrix(&MatrixString, matRow, matCol);
+//     for (i = 0; i < matRow; i++){
+//         for (j = 0; j < matCol; j++){
+//             scanf("%s", word);
+//             insertLastStrArr(&MatrixString.buffer[i], word);
+//         }
+//     }
+//     displayMatrix(MatrixString);
+
+//     createPointArray(&ArrayPoint, arrLen);
+//     ArrayPoint.buffer[0].X = 1;
+//     ArrayPoint.buffer[0].Y = 2;
+//     ArrayPoint.buffer[1].X = 3;
+//     ArrayPoint.buffer[1].Y = 4;
+//     ArrayPoint.buffer[2].X = 5;
+//     ArrayPoint.buffer[2].Y = 6;
+//     printf("%d %d\n", ArrayPoint.buffer[0].X, ArrayPoint.buffer[0].Y);
+//     printf("%d %d\n", ArrayPoint.buffer[1].X, ArrayPoint.buffer[1].Y);
+//     printf("%d %d\n", ArrayPoint.buffer[2].X, ArrayPoint.buffer[2].Y);
 
 //     return 0;
 // }
--- src/cyberpunkhack.h
+++ src/cyberpunkhack.h
@@ -4,10 +4,18 @@
 #include <stdio.h>
 #include <string.h>
 
+/* Definisi tipe data dynamic array of integer */
+typedef struct{
+    int *buffer;
+    int nEff;
+    int maxLen;
+} intArray;
+
 /* Definisi tipe data dynamic array of string */
 typedef struct{
     char **buffer;
     int nEff;
+    int maxLen;
 } strArray;
 
 /* Definisi tipe data dynamic matrix of string */
@@ -23,21 +31,46 @@ typedef struct{
     int top;
 } strStack;
 
-/* Konstruktor */
-void createArray(strArray *Arr, int length);
-/* I.S. sembarang */
-/* F.S. terbentuk dynamic array of string Arr dengan panjang nEff */
+/* Definisi tipe data point */
+typedef struct{
+    int X;
+    int Y;
+} point;
 
-void createMatrix(strMatrix *Mat, int height, int width);
+/* Definisi tipe data array of point */
+typedef struct{
+    point *buffer;
+    int nEff;
+    int maxLen;
+} pointArray;
+
+/* Konstruktor */
+void createIntArray(intArray *Arr, int length);
 /* I.S. sembarang */
-/* F.S. terbentuk dynamic matrix of string Mat dengan baris rowEff dan kolom colEff */
+/* F.S. terbentuk dynamic array of integer Arr dengan panjang length */
+
+void createStringArray(strArray *Arr, int length);
+/* I.S. sembarang */
+/* F.S. terbentuk dynamic array of string Arr dengan panjang length */
+
+void createStringMatrix(strMatrix *Mat, int height, int width);
+/* I.S. sembarang */
+/* F.S. terbentuk dynamic matrix of string Mat dengan baris height dan kolom width */
 
 void createStack(strStack *Stack, int capacity);
 /* I.S. sembarang */
 /* F.S. terbentuk stack of string Stack dengan ukuran capacity */
 
+void createPoint(point *P, int X, int Y);
+/* I.S. sembarang */
+/* F.S. terbentuk point P dengan koordinat (X, Y) */
+
+void createPointArray(pointArray *Arr, int length);
+/* I.S. sembarang */
+/* F.S. terbentuk dynamic array of point Arr dengan panjang length */
+
 /* Operator array, matrix, dan stack */
-void insertLast(strArray *Arr, char *str);
+void insertLastStrArr(strArray *Arr, char *str);
 /* I.S. strArray terdefinisi, mungkin kosong */
 /* F.S. str ditambahkan sebagai elemen terakhir strArray */
 
@@ -49,12 +82,16 @@ void pop(strStack *Stack, char *str);
 /* I.S. strStack Stack terdefinisi, tidak kosong */
 /* F.S. string str dihapus dari stack of string Stack*/
 
-void displayArray(strArray *Arr);
+void displayArray(strArray Arr);
 /* I.S. strArray Arr terdefinisi, mungkin kosong */
 /* F.S. tampil pada layar strArray Arr */
 
-void displayMatrix(strMatrix *Mat);
+void displayMatrix(strMatrix Mat);
 /* I.S. strMatrix Mat terdefinisi, mungkin kosong */
 /* F.S. tampil pada layar strMatrix Mat */
+
+void displaySeqAndReward(strMatrix Mat, intArray Seq, intArray Reward);
+/* I.S. strMatrix Mat, intArray Seq, dan intArray Reward terdefinisi */
+/* F.S. tampil pada layar daftar sequence dan rewardnya */
 
 #endif
